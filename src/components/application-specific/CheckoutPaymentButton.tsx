c2189b5a8d1bb07157fb0c2cfb0d9b347397af11
renanfeluck <renanfeluck@gmail.com>
2021-03-25 10:01:59 -0300

    Checkout Page

---
+++ src/components/application-specific/CheckoutPaymentButton.tsx
@@ -0,0 +1,42 @@
+import styled from 'styled-components';
+
+const CheckboxLabel = styled.label`
+  font-family: Open Sans;
+  font-style: normal;
+  font-weight: normal;
+  font-size: 16px;
+  line-height: 22px;
+
+  color: #535353;
+
+  display: flex;
+  justify-content: space-between;
+  flex: 1;
+
+  &:checked {
+    border: 1px solid red;
+  }
+
+  [type='radio'] {
+    display: none;
+  }
+`;
+
+const PaymentButtonBox = styled.div<{ checked?: boolean }>`
+  background: #ffffff;
+  border: ${props => (props.checked ? '1px solid #5dac50' : '')};
+  border-radius: 10.8766px;
+  width: calc(100% - 48px);
+  padding: 24px;
+  margin-bottom: 15px;
+`;
+
+const CheckoutPaymentButton = ({ children, checked }) => {
+  return (
+    <PaymentButtonBox checked={checked}>
+      <CheckboxLabel>{children}</CheckboxLabel>
+    </PaymentButtonBox>
+  );
+};
+
+export default CheckoutPaymentButton;
--- src/pages/checkout/index.tsx
+++ src/pages/checkout/index.tsx
@@ -3,6 +3,9 @@ import Stepper from '@app/Stepper';
 import Container from '@design/Container';
 import styled from 'styled-components';
 import Image from 'next/image';
+import CheckoutPaymentButton from '@app/CheckoutPaymentButton';
+import { useState } from 'react';
+import Button from '@design/Button';
 
 const CheckoutView = styled.div`
   display: flex;
@@ -25,9 +28,53 @@ const CheckoutDetails = styled.div`
   background: #f7f7f7;
   border-radius: 10.8766px;
   padding: 30px;
+  flex-direction: row;
+  display: flex;
+  flex-wrap: wrap;
+`;
+
+const CheckoutDetailsInfo = styled.div`
+  flex: 50%;
+  width: 50%;
+`;
+
+const CheckoutTitle = styled.p`
+  font-family: Open Sans;
+  font-style: normal;
+  font-weight: normal;
+  font-size: 22px;
+  line-height: 30px;
+  color: #000000;
+`;
+
+const CheckoutGrayText = styled.p`
+  font-family: Open Sans;
+  font-style: normal;
+  font-weight: normal;
+  font-size: 16px;
+  line-height: 153.3%;
+  letter-spacing: 0.612903px;
+  color: #979797;
+`;
+
+const PriceText = styled.span`
+  font-family: Open Sans;
+  font-style: normal;
+  font-weight: normal;
+  font-size: 43.5063px;
+  line-height: 44px;
+  color: #000000;
+  width: 50%;
+  text-align: right;
+`;
+
+const PaymentBox = styled.div`
+  width: 100%;
 `;
 
 const Checkout = (): JSX.Element => {
+  const [paymentMethod, setPaymentMethod] = useState('bank');
+
   return (
     <>
       <Header />
@@ -41,7 +88,68 @@ const Checkout = (): JSX.Element => {
               objectFit="cover"
             />
           </CheckoutImage>
-          <CheckoutDetails> Detais</CheckoutDetails>
+          <CheckoutDetails>
+            <CheckoutDetailsInfo>
+              <CheckoutTitle>Cart total</CheckoutTitle>
+
+              <p>SS Sneaker</p>
+
+              <CheckoutGrayText>x 1 Green Size 41 Item #2839u512401</CheckoutGrayText>
+            </CheckoutDetailsInfo>
+            <CheckoutDetailsInfo>
+              <CheckoutTitle>Delivery details</CheckoutTitle>
+
+              <CheckoutGrayText>
+                John Smith Phone no: 01312428200 Address: Redwood City, 2000.
+              </CheckoutGrayText>
+              <CheckoutDetails>
+                <div>
+                  <p>Total cost</p>
+                  <CheckoutGrayText>Delivery included</CheckoutGrayText>
+                </div>
+
+                <PriceText>$100</PriceText>
+              </CheckoutDetails>
+            </CheckoutDetailsInfo>
+            <PaymentBox>
+              <CheckoutTitle> Select your payment method </CheckoutTitle>
+              <form>
+                <CheckoutPaymentButton checked={paymentMethod === 'bank'}>
+                  Online Banking <Image src="/banks.png" alt="" width="176px" height="29px" />
+                  <input
+                    type="radio"
+                    name="method"
+                    value="bank"
+                    checked={paymentMethod === 'bank'}
+                    onChange={() => setPaymentMethod('bank')}
+                  />
+                </CheckoutPaymentButton>
+                <CheckoutPaymentButton checked={paymentMethod === 'cards'}>
+                  Card Payment <Image src="/cards.png" alt="" width="85px" height="24px" />
+                  <input
+                    type="radio"
+                    name="method"
+                    value="cards"
+                    checked={paymentMethod === 'cards'}
+                    onChange={() => setPaymentMethod('cards')}
+                  />
+                </CheckoutPaymentButton>
+
+                <CheckoutPaymentButton checked={paymentMethod === 'apple'}>
+                  Apple Pay <Image src="/applePay.png" alt="" width="66px" height="35px" />
+                  <input
+                    type="radio"
+                    name="method"
+                    value="apple"
+                    checked={paymentMethod === 'apple'}
+                    onChange={() => setPaymentMethod('apple')}
+                  />
+                </CheckoutPaymentButton>
+
+                <Button> Continue </Button>
+              </form>
+            </PaymentBox>
+          </CheckoutDetails>
         </CheckoutView>
       </Container>
     </>
